20061ef419bd2ba62de6651f4a153330c1513451
Ayush-Manware <ayushmanware19@gmail.com>
2023-11-10 21:02:18 +0530

    listen method

--- index.js
+++ index.js
@@ -26,39 +26,3 @@ const server = HTTP.createServer((req, res)=>{
 server.listen(5055, ()=>{
     console.log('Server is live')
 })
-
-
-
-
-
-
-
-
-// console.log("Hello")
-// console.log(22)
-// const HTTP = require('http')
-// const jsonData = require('./jsonPackage')
-
-// const server = HTTP.createServer((req, res)=>{
-//     if(req.url === '/'){
-//         res.write('<p>"/" slash Route</p>')
-//         res.end()
-//     }
-//     else if(req.url === '/product'){
-//         const store = JSON.stringify(jsonData)
-//         res.write(store)
-//         res.end()
-//     }
-//     else if(req.url === '/service'){
-//         res.write('<p>Service Route<p>')
-//         res.end()
-//     }
-//     else{
-//         res.write('<p>No Routes Matched</p>')
-//         res.end()
-//     }
-// })
-
-// server.listen(555, ()=>{
-//     console.log('Server Started')
-// })
